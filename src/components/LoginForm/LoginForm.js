d819ae416985ec7b9036a388eb006b4fff86f54d
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-08-20 13:14:40 +0300

    minor fixes

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { LoginContext } from '../../context/LoginContext';
 import Input from '../Input/Input';
