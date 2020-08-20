8ea6609ec90cb34159b52b6f254686d8a258eefe
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-08-20 13:21:53 +0300

    Updates

---
+++ precache-manifest.77a2acd8a86c6d7bc5aaf7b9f042b6bc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21e54ade60bbf0dde4de9fd8bccb185",
+    "url": "/vsemeda-test-task/index.html"
+  },
+  {
+    "revision": "503792bd7e1e3503614e",
+    "url": "/vsemeda-test-task/static/css/main.bc5a1f45.chunk.css"
+  },
+  {
+    "revision": "d5261e580860bfb089c1",
+    "url": "/vsemeda-test-task/static/js/2.83996c5a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/vsemeda-test-task/static/js/2.83996c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503792bd7e1e3503614e",
+    "url": "/vsemeda-test-task/static/js/main.1f07d7d8.chunk.js"
+  },
+  {
+    "revision": "9de943b92f5b0d5137cc",
+    "url": "/vsemeda-test-task/static/js/runtime-main.7f114b37.js"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/vsemeda-test-task/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/vsemeda-test-task/static/media/SF-ProText-Regular.85bd46c1.ttf"
+  },
+  {
+    "revision": "2dbb243f5a800e35e969173602bbe133",
+    "url": "/vsemeda-test-task/static/media/address-icon.2dbb243f.svg"
+  },
+  {
+    "revision": "3cb6d5f602f7915664983a62393c805f",
+    "url": "/vsemeda-test-task/static/media/appstore-icon.3cb6d5f6.svg"
+  },
+  {
+    "revision": "3953101fb5528519c6ebe9ffd793bb57",
+    "url": "/vsemeda-test-task/static/media/car-icon.3953101f.svg"
+  },
+  {
+    "revision": "4e4f307350bff796de0eba26b76f0f51",
+    "url": "/vsemeda-test-task/static/media/cart-icon.4e4f3073.svg"
+  },
+  {
+    "revision": "7b9ab84a34fc98e98b6664a7ce4179c3",
+    "url": "/vsemeda-test-task/static/media/cross-icon.7b9ab84a.svg"
+  },
+  {
+    "revision": "b401687b6912e6a49a080458ba5f7da9",
+    "url": "/vsemeda-test-task/static/media/delivery-icon.b401687b.svg"
+  },
+  {
+    "revision": "cc21d80d9d52eebc42b677d3861f52fe",
+    "url": "/vsemeda-test-task/static/media/facebook-icon.cc21d80d.svg"
+  },
+  {
+    "revision": "ab7441915aacfdb406e6a55394bb6707",
+    "url": "/vsemeda-test-task/static/media/googleplay-icon.ab744191.svg"
+  },
+  {
+    "revision": "7b8bb17cbee760a772aff18b3e11e82d",
+    "url": "/vsemeda-test-task/static/media/instagram-icon.7b8bb17c.svg"
+  },
+  {
+    "revision": "92c6d88831ef9bc61f0a0084a017b7d7",
+    "url": "/vsemeda-test-task/static/media/left-arrow-icon.92c6d888.svg"
+  },
+  {
+    "revision": "2275fdab17e57330696878d1ca9d4323",
+    "url": "/vsemeda-test-task/static/media/like-icon.2275fdab.svg"
+  },
+  {
+    "revision": "a9a512298fa6fe64468d78ac42397282",
+    "url": "/vsemeda-test-task/static/media/login-icon.a9a51229.svg"
+  },
+  {
+    "revision": "e9816ad9a261b9a7b71d103e636f437c",
+    "url": "/vsemeda-test-task/static/media/logo-colorless.e9816ad9.svg"
+  },
+  {
+    "revision": "ba47632848bdff5c88feed70128c757a",
+    "url": "/vsemeda-test-task/static/media/logo.ba476328.svg"
+  },
+  {
+    "revision": "99ef9aa8fe6e365feda9d5d56fb3826d",
+    "url": "/vsemeda-test-task/static/media/logout-icon.99ef9aa8.svg"
+  },
+  {
+    "revision": "142ec1d62546c8510cf5e9e8323cbb26",
+    "url": "/vsemeda-test-task/static/media/money-icon.142ec1d6.svg"
+  },
+  {
+    "revision": "1de009857a5f865044792d9737374efb",
+    "url": "/vsemeda-test-task/static/media/more-icon.1de00985.svg"
+  },
+  {
+    "revision": "733d1585143589022d1f3156b7cccd13",
+    "url": "/vsemeda-test-task/static/media/orders-icon.733d1585.svg"
+  },
+  {
+    "revision": "a44d461213f3adc11ca9f2f2186cd1fa",
+    "url": "/vsemeda-test-task/static/media/right-arrow-icon.a44d4612.svg"
+  },
+  {
+    "revision": "dd35198677488a5cf8fe196a3f95b7d6",
+    "url": "/vsemeda-test-task/static/media/search-icon.dd351986.svg"
+  },
+  {
+    "revision": "320b6e2c6ae91272b46947d72e5c1560",
+    "url": "/vsemeda-test-task/static/media/time-icon.320b6e2c.svg"
+  },
+  {
+    "revision": "7f2ef86e74c6eedd8c5f3923ad2197e7",
+    "url": "/vsemeda-test-task/static/media/user-icon.7f2ef86e.svg"
+  },
+  {
+    "revision": "74ef059e5efc2889f64f77e34b783ca9",
+    "url": "/vsemeda-test-task/static/media/vk-icon.74ef059e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dabf3f3015561f2da4c252a3d507a8a5.js"
+  "/vsemeda-test-task/precache-manifest.77a2acd8a86c6d7bc5aaf7b9f042b6bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vsemeda-test-task/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
